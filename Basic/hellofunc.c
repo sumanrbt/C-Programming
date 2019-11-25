9706b0c1343d6123c8a65c42777a312b08f199b0
suman <sumanrbt1995@gmail.com>
2019-11-25 12:34:43 +0530

    added a hellofun.c file

---
+++ Basic/hellofunc.c
@@ -0,0 +1,10 @@
+/* Here we are going to create a a simple function to print a defined text, the main aim of this tutorial is to learn about Makefiles in C*?
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#include <hellomake.h>
+int main(){
+printf("Hello SUMAN, Makefiles!\n");
+return 0;
+}
